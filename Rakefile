047fb1a6c7fb0fc6e3c65ef2098811428655331e
beekley <brettbeekley@gmail.com>
2021-12-11 11:57:13 -0800

    Use Google Sheet for card data.

--- Rakefile
+++ Rakefile
@@ -3,5 +3,6 @@ require 'squib'
 task default: [:deck]
 
 task :deck do
+  sh "curl 'https://docs.google.com/spreadsheets/d/1SV8VB8KNjVy2acICk6ZNY3HaWjszx8Pu-WrdYQ5hsEM/export?format=csv'  -L -o data.csv"
   load 'deck.rb'
 end
--- deck.rb
+++ deck.rb
@@ -1,6 +1,12 @@
 require 'squib'
 
-Squib::Deck.new(cards: 3, layout: 'layout.yml') do
-  text str: 'Hello, World!'
-  save format: :png
+data = Squib.csv file: 'data.csv'
+
+Squib::Deck.new cards: data['name'].size, layout: 'economy.yml' do
+  background color: 'white'
+  rect layout: 'cut' # cut line as defined by TheGameCrafter
+  rect layout: 'safe' # safe zone as defined by TheGameCrafter
+  text str: data['name'], layout: 'title'
+  text str: data['power'], layout: 'description'
+  save_png
 end
