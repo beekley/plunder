e22c77d78f427e465d26cf29647f5a6c3b5f8220
beekley <brettbeekley@gmail.com>
2021-12-20 16:25:06 -0800

    Add autobuild setting and build ID

--- src/deck.rb
+++ src/deck.rb
@@ -8,9 +8,10 @@ Squib::Deck.new cards: ship_data['title'].size, layout: 'src/ship.yml' do
   background color: 'white'
   rect layout: 'cut' # cut line as defined by TheGameCrafter
   rect layout: 'safe' # safe zone as defined by TheGameCrafter
+  text str: "last built: #{Time.now}", layout: 'build'
 
   # Descriptive elements.
-  text str: ship_data['title'],layout: 'title'
+  text str: ship_data['title'], layout: 'title'
   text str: ship_data['description'], layout: 'description'
   # We have to use `map` here to combine the "power" column with the "toughness" column.
   # The result is `power/toughness`. There might be an easier way to do this.
