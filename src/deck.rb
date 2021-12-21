ad55304b6688aee1b4459f8004d5082e1262a9ad
beekley <brettbeekley@gmail.com>
2021-12-20 16:03:05 -0800

    Add elements to card

--- src/deck.rb
+++ src/deck.rb
@@ -1,12 +1,26 @@
 require 'squib'
 
-data = Squib.csv file: '../cardData/ships.csv'
+ship_data = Squib.csv file: 'cardData/ships.csv'
 
-Squib::Deck.new cards: data['name'].size, layout: 'economy.yml' do
+# Generate ship cards.
+Squib::Deck.new cards: ship_data['title'].size, layout: 'src/ship.yml' do
+  # Card layout.
   background color: 'white'
   rect layout: 'cut' # cut line as defined by TheGameCrafter
   rect layout: 'safe' # safe zone as defined by TheGameCrafter
-  text str: data['name'], layout: 'title'
-  text str: data['power'], layout: 'description'
+
+  # Descriptive elements.
+  text str: ship_data['title'],layout: 'title'
+  text str: ship_data['description'], layout: 'description'
+  # We have to use `map` here to combine the "power" column with the "toughness" column.
+  # The result is `power/toughness`. There might be an easier way to do this.
+  text str: ship_data['power'].map.with_index{ |x, i| "#{x}/#{ship_data['toughness'][i]}" },
+    layout: 'snark'
+
+  # Slots.
+  text str:
+    ship_data['slot1_location'].map.with_index{ |x, i| "#{x}\n#{ship_data['slot1_type'][i]}" },
+    layout: 'bonus1'
+
   save_png
 end
