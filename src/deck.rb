78b63ef7fa169e03b722b01fd2575c95a6749d45
beekley <brettbeekley@gmail.com>
2021-12-20 16:42:24 -0800

    Add all slots

--- src/deck.rb
+++ src/deck.rb
@@ -14,14 +14,22 @@ Squib::Deck.new cards: ship_data['title'].size, layout: 'src/ship.yml' do
   text str: ship_data['title'], layout: 'title'
   text str: ship_data['description'], layout: 'description'
   # We have to use `map` here to combine the "power" column with the "toughness" column.
-  # The result is `power/toughness`. There might be an easier way to do this.
-  text str: ship_data['power'].map.with_index{ |x, i| "#{x}/#{ship_data['toughness'][i]}" },
+  text str: ship_data['power'].map.with_index { |x, i|
+      # "power/toughness".
+      "#{x}/#{ship_data['toughness'][i]}"
+    },
     layout: 'snark'
 
   # Slots.
-  text str:
-    ship_data['slot1_location'].map.with_index{ |x, i| "#{x}\n#{ship_data['slot1_type'][i]}" },
-    layout: 'bonus1'
+  for slot_i in 1..5 do
+    text str:
+      ship_data["slot#{slot_i}_location"].map.with_index { |x, i|
+        unless x.to_s.strip.empty?
+          "#{x} slot\n#{ship_data["slot#{slot_i}_type"][i]}"
+        end
+      },
+      layout: "bonus#{slot_i}"
+   end
 
   save_png
 end
